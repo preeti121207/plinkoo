9b5605561af7f0dd03bf047c81524d2516738ea4
preeti121207 <69007432+preeti121207@users.noreply.github.com>
2020-12-24 08:54:46 -0800

    Add files via upload

---
+++ Plinko.js
@@ -0,0 +1,17 @@
+class Plinko {
+    constructor(x,y,radius) {
+      var options = {
+          isStatic: true
+      }
+      this.body = Bodies.circle(x,y,10,options);
+      this.radius = radius;
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      ellipseMode(RADIUS);
+      fill("white");
+      ellipse(pos.x, pos.y, 10, 10);
+    }
+  };
+  
